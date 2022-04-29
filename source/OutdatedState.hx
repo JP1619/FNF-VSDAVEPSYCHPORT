096800f14d9ff409e02abeb05e457f4945a07d0f
JP1619 <89480340+JP1619@users.noreply.github.com>
2022-04-29 20:03:24 -0300

    Update OutdatedState.hx

--- source/OutdatedState.hx
+++ source/OutdatedState.hx
@@ -41,7 +41,6 @@ class OutdatedState extends MusicBeatState
 		if(!leftState) {
 			if (controls.ACCEPT) {
 				leftState = true;
-				CoolUtil.browserLoad("https://github.com/ShadowMario/FNF-PsychEngine/releases");
 			}
 			else if(controls.BACK) {
 				leftState = true;
